48ad9398cb773f3d60c145f75d02539e39200585
Jeroen Bos <jeroen@deepinsight.io>
2020-03-08 14:49:33 +0100

    breakpoint on line 60 triggers when I move the slider!

--- app/src/main/java/com/example/slidercontrol/MainActivity.java
+++ app/src/main/java/com/example/slidercontrol/MainActivity.java
@@ -2,7 +2,16 @@ package com.example.slidercontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -14,9 +23,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        Intent mode = new Intent("com.oem.intent.action.THREE_KEY_MODE");
         FileSystem fs = FileSystems.getDefault();
         Path path = fs.getPath("/sys/class/switch/tri-state-key/state");
         String contents;
@@ -25,12 +36,51 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException ex) {
             contents = ex.getClass().getSimpleName() + ": " + ex.getMessage();
         }
-
+        String s = Settings.Global.getString(getContentResolver(), "three_Key_mode");
         setContentView(R.layout.activity_main);
 
 
         TextView textView = (TextView) findViewById(R.id.textview);
-        textView.setText(contents);
+        textView.setText(contents + "\n" + s);
+//        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+
+
+        ContentResolver contentResolver = getContentResolver();
+        Uri setting = Settings.Global.getUriFor("three_Key_mode");
+
+        // Make a listener
+        ContentObserver observer = new ContentObserver(new Handler()) {
+            @Override
+            public void onChange(boolean selfChange) {
+                super.onChange(selfChange);
+
+                TextView textView = (TextView) findViewById(R.id.textview);
+                i++;
+                textView.setText(String.valueOf(i));
+
+
+            }
+
+            @Override
+            public boolean deliverSelfNotifications() {
+                return true;
+            }
+        };
+
+// Start listening
+
+        contentResolver.registerContentObserver(setting, false, observer);
+
+// Stop listening
+//        contentResolver.unregisterContentObserver(observer);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        String s = Settings.Global.getString(getContentResolver(), "three_Key_mode");
+        setContentView(R.layout.activity_main);
 
     }
 }

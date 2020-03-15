dcfcc5b485855161d5f4b3ac94d4d2b18e077b57
Jeroen Bos <jeroen@deepinsight.io>
2020-03-15 01:00:15 +0100

    Crash was due to new install requiring renewed permissions. But besides, I realized you don't need an icon. It's always there in the 'all apps' list

--- app/src/main/java/com/example/slidercontrol/MainActivity.java
+++ app/src/main/java/com/example/slidercontrol/MainActivity.java
@@ -102,7 +102,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void registerThreeKeyModeListener() {
-
         // Make a listener
         ContentObserver observer = new ContentObserver(new Handler()) {
             @Override
